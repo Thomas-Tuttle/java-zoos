4201c2dcf33982d74a2b409e9c41f9ed9a493537
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-06-13 18:01:44 -0400

    Completed Models

--- zoo/src/main/java/com/lambdaschool/zoo/model/Phone.java
+++ zoo/src/main/java/com/lambdaschool/zoo/model/Phone.java
@@ -1,5 +1,90 @@
 package com.lambdaschool.zoo.model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "phone")
 public class Phone
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long phoneid;
+    private String phonetype;
+    private String phonenumber;
+
+    @ManyToOne
+    @JoinColumn(name = "zooid")
+    private Zoo zoo;
+
+    @ManyToMany(mappedBy = "phones")
+    private List<Animal> animals = new ArrayList<>();
+
+    public Phone()
+    {
+
+    }
+
+    public Phone(String phonetype)
+    {
+        this.phonetype = phonetype;
+    }
+
+    public Phone(String phonetype, Zoo zoo)
+    {
+        this.phonetype = phonetype;
+        this.zoo = zoo;
+    }
+
+    public long getPhoneid()
+    {
+        return phoneid;
+    }
+
+    public void setPhoneid(long phoneid)
+    {
+        this.phoneid = phoneid;
+    }
+
+    public String getPhonetype()
+    {
+        return phonetype;
+    }
+
+    public void setPhonetype(String phonetype)
+    {
+        this.phonetype = phonetype;
+    }
+
+    public String getPhonenumber()
+    {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber)
+    {
+        this.phonenumber = phonenumber;
+    }
+
+    public Zoo getZoo()
+    {
+        return zoo;
+    }
+
+    public void setZoo(Zoo zoo)
+    {
+        this.zoo = zoo;
+    }
+
+    public List<Animal> getAnimals()
+    {
+        return animals;
+    }
+
+    public void setAnimals(List<Animal> animals)
+    {
+        this.animals = animals;
+    }
 }
--- zoo/src/main/java/com/lambdaschool/zoo/model/Zoo.java
+++ zoo/src/main/java/com/lambdaschool/zoo/model/Zoo.java
@@ -1,6 +1,8 @@
 package com.lambdaschool.zoo.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "zoo")
@@ -8,6 +10,49 @@ public class Zoo
 {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     private long zooid;
     private String zooname;
+
+    @OneToMany(mappedBy = "zoo")
+    private List<Phone> phones = new ArrayList<>();
+
+    public Zoo()
+    {
+    }
+
+    public Zoo(String zooname)
+    {
+        this.zooname = zooname;
+    }
+
+    public long getZooid()
+    {
+        return zooid;
+    }
+
+    public void setZooid(long zooid)
+    {
+        this.zooid = zooid;
+    }
+
+    public String getZooname()
+    {
+        return zooname;
+    }
+
+    public void setZooname(String zooname)
+    {
+        this.zooname = zooname;
+    }
+
+    public List<Phone> getPhones()
+    {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones)
+    {
+        this.phones = phones;
+    }
 }
